eef67854fb3363f8a821dee9e81ce0e6e85dc528
Larry Clyman <lclyman@gmail.com>
2018-06-10 07:34:33 -0600

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,7 @@ import * as mutations  from './mutations';
 Vue.use(Vuex)
 // const API_KEY = '<< your National Park Service api key here >>'
 // const WEATHER_API_KEY = '<< your OpenWeather api key here >>'
-// const NPS_API_KEY = 'coa45xILoSovnptVgMsshu0XP1oUs13jB0LkH0Zv'
-const NPS_API_KEY = 'l0JUo7zsJwenNuTztSuGOGxj1Y2LlW1acxvJvKA0'
-const OW_API_KEY = '395df36cfef9b638b1c632f7a32a4b0d'
+// const NPS_API_KEY = '<< your NPS api key here >>'
 
 export const store = new Vuex.Store({
   state: {
