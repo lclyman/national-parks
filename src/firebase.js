85d50101d41b738467aa1ec407ab1d45cbcd9ef9
Larry Clyman <lclyman@gmail.com>
2018-06-10 07:33:05 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 firebase.initializeApp({
-  projectId: 'vue-firestore-a1010',
-  databaseURL: 'https://vue-firestore-a1010.firebaseio.com'
+  projectId: '<<your firebase id>>',
+  databaseURL: '<<your URL>>'
 })
 export const db = firebase.firestore()
